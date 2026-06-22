350dcf2978f3cf050cd02c92bbcb7d20c6370398
potatogodh <harrison.garlick01@gmail.com>
2026-06-22 19:11:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    # Shuffle once and send to frontend
     shuffled = pokemon_list[:]
     random.shuffle(shuffled)
     return render_template("index.html", pokemon=shuffled)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
